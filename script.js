7e285064159a1eb7166f5595708f34d1f5f6a1c8
Sahaj Singh <Sahaj@vt.edu>
2024-07-31 14:14:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
-
+import { createClient } from "../node-modules/@supabase/supabase-js";
 const supabaseUrl = 'https://vrszetgxfqizfjbrtscw.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
